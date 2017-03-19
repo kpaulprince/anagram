b98dea0d9afa1beae19fb106b975952262272967
prince <kpaulprince@gmail.com>
2017-03-18 20:09:46 -0700

    added main, fixed code

--- check_for_anagram.py
+++ check_for_anagram.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
-length = raw_input('Enter length of list: ')
 
-l = []
-for item in range(int(length)):
-    l.append(raw_input('Enter element #{}: '.format(item)))
+class Anagram():
+    def __init__(self):
+        pass
 
-def compare(x,y):
-    temp_y = y
-    for c in x:
-        temp_y = temp_y.replace(c,'')
-    if len(temp_y) == 0:
-        print '{} is anagram with {}'.format(x,y)
+    def get_list(self):
+        l = []
+        length = raw_input('Enter length of list: ')
+        for item in range(int(length)):
+            l.append(raw_input('Enter element #{}: '.format(item)))
+        return l
 
-for i,e1 in enumerate(l):
-    for e2 in l[i+1:]:
-        if len(e1) == len(e2):
-            compare(e1,e2)
+    def compare(self,x,y):
+        temp_y = y
+        for c in x:
+            temp_y = temp_y.replace(c,'',1)
+        if len(temp_y) == 0:
+            print '{} is anagram with {}'.format(x,y)
+
+    def check_for_anagram(self,l):
+        for i,e1 in enumerate(l):
+            for e2 in l[i+1:]:
+                if len(e1) == len(e2):
+                    self.compare(e1,e2)
+
+if __name__ == "__main__":
+    a = Anagram()
+    a.check_for_anagram(a.get_list())
